308d325a709b00b2f6480e4de44c66f646210ebf
shrinath <virusking045@gmail.com>
2026-02-21 16:15:15 +0530

    test clasess added

---
+++ src/test/java/in/ashokit/AppTest.java
@@ -0,0 +1,25 @@
+package in.ashokit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testGetMessage() {
+        App app = new App();
+        assertEquals("Hello World!", app.getMessage());
+    }
+}
